8ad9ed6ce54c62f45d9abbfd2627afb39625f693
Alex31y <83077112+Alex31y@users.noreply.github.com>
2022-08-08 01:50:35 +0200

    un grafo

--- app.js
+++ app.js
@@ -1,53 +1,34 @@
-<script>
+import * as d3 from "d3";
 
+let data = [31, 42, 56, 92, 84, 72, 53, 43, 29, 24, 64, 49];
 
-// set the dimensions and margins of the graph
-var margin = {top: 30, right: 30, bottom: 70, left: 60},
-    width = 460 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
+const barWidth = 75;
+const barOffset = 10;
+const height = 600;
+const width = 800;
 
-// append the svg object to the body of the page
-var svg = d3.select("#")
-  .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform",
-          "translate(" + margin.left + "," + margin.top + ")");
+let yScale = d3.scaleLinear()
+    .domain([0, d3.max(data)])
+    .range([0, height]);
 
-// Parse the Data
-d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/7_OneCatOneNum_header.csv", function(data) {
-
-// X axis
-var x = d3.scaleBand()
-  .range([ 0, width ])
-  .domain(data.map(function(d) { return d.Country; }))
-  .padding(0.2);
-svg.append("g")
-  .attr("transform", "translate(0," + height + ")")
-  .call(d3.axisBottom(x))
-  .selectAll("text")
-    .attr("transform", "translate(-10,0)rotate(-45)")
-    .style("text-anchor", "end");
-
-// Add Y axis
-var y = d3.scaleLinear()
-  .domain([0, 13000])
-  .range([ height, 0]);
-svg.append("g")
-  .call(d3.axisLeft(y));
-
-// Bars
-svg.selectAll("mybar")
-  .data(data)
-  .enter()
-  .append("rect")
-    .attr("x", function(d) { return x(d.Country); })
-    .attr("y", function(d) { return y(d.Value); })
-    .attr("width", x.bandwidth())
-    .attr("height", function(d) { return height - y(d.Value); })
-    .attr("fill", "#69b3a2")
-
-})
-
-</script>
+d3.select('#bar-graph')
+    .append('svg')
+        .attr('width', width)
+        .attr('height', height)
+        .style('background', '#3A742C')
+    .selectAll('rect')
+        .data(data)
+        .enter()
+        .append('rect')
+            .style('fill', '#30A08B')
+            .attr('width', barWidth)
+            .attr('height', (d) => {
+                return yScale(d);
+            })
+            .attr('x', (d, i) => {
+                return i * (barWidth + barOffset);
+            })
+            .attr('y', (d) => {
+                return height - yScale(d);
+            })
+    ;
